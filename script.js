e88033d3a72ac838d5cb94b4608a6516b4e8c33c
sagar <87647770+raut07sagar@users.noreply.github.com>
2021-11-18 10:07:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,104 @@
-
-var final = document.createElement('div');
-final.setAttribute('class','container-fluid');
-
-var box = document.createElement('div');
-box.setAttribute('class','box');
+// creating a element with div for tag and search box
+const container1 = document.createElement('div')
+container1.className = 'container'
 
 
+const heading = document.createElement('h1')
+heading.innerHTML = 'Search your product here !'
+container1.appendChild(heading)
+document.body.appendChild(container1)
+
+
+const container2 = document.createElement('div')
+container2.className = 'container'
+
+const product_type = document.createElement('input')
+const search = document.createElement('button')
+search.innerHTML = 'SEARCH HERE'
+container2.appendChild(product_type)
+container2.appendChild(search)
+document.body.appendChild(container2)
+const container3 = document.createElement('div')
+container3.className = 'container3'
 
 
 
+// creating a function to get value from backend 
 
+async function getProducts() {
 
-
- 
- async function getdata(){
-  try
-  {
-   
-    var data = await fetch("https://makeup-api.herokuapp.com/api/v1/products.json");
-    var datares = await data.json();
+    let type = product_type.value
     
+    // reset
+    container3.innerHTML = ''
 
-    // for(let i=0;i<15;i++){
-    //     var res = datares[i].brand+" "+datares[i].name+" "+datares[i].price+" "+datares[i].api_featured_image+" "+datares[i].product_link+" "+datares[i].description ;
-    //     console.log(res)
-    // }
-
-    for(let i=0;i<datares.length;i++){
-
-      var image = document.createElement('img');
-      image.setAttribute('src',datares[i].image_link);
-
- var brand = document.createElement('h3');
- brand.setAttribute('class','cname');
- brand.innerHTML='Brand :'+' '+datares[i].brand;
-
- var name1 = document.createElement('h3');
- name1.setAttribute('class','cname1');
- name1.innerHTML='Name :'+' '+datares[i].name;
-
- var price = document.createElement('h3');
- price.setAttribute('class','cname2');
- price.innerHTML='Price :'+' '+datares[i].price;
-
- var product = document.createElement('h3');
- product.setAttribute('class','cname3');
- product.innerHTML='Product Link :'+' '+datares[i].product_link;
-
- var description = document.createElement('h3');
- description.setAttribute('class','cname4');
- description.innerHTML='Description :'+' '+datares[i].description;
- 
- 
+    product_type.value = ''
 
 
- box.append(image,brand,name1,price,product,description);
-  
- final.append(box);
- document.body.append(final);
+   //   fetching the product from api
+    const response = await fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}`)
 
-    }
-  }
-  catch(error)
-  {
-   console.log(error)
-  }
+    const data = await response.json()
+
+
+    console.log(data)
+
+    appendData(data)
+
+}
+//creating a function to print from backend to front end
+function appendData(data) {
+
+
+    data.forEach(item => {
+
+        const smallbox = document.createElement('div')
+
+        smallbox.className = 'small-container'
+
+        const images = document.createElement('img')
+        const names = document.createElement('h2')
+        const brands = document.createElement('h3')
+        const prices = document.createElement('h3')
+        const product_link = document.createElement('a')
+        const description = document.createElement('p')
+
+
+
+
+        images.src = item.image_link
+        names.innerHTML = item.name
+        brands.innerHTML = `Brand : ${item.brand}`
+
+        prices.innerHTML = `Price : $${item.price}`
+        // link for product
+        product_link.href = item.product_link
+        product_link.innerHTML = 'Check the Product'
+        description.innerHTML = item.description
+
+
+
+        smallbox.appendChild(images)
+        smallbox.appendChild(names)
+        smallbox.appendChild(brands)
+        smallbox.appendChild(prices)
+
+        smallbox.appendChild(product_link)
+        smallbox.appendChild(description)
+
+
+
+
+        container3.appendChild(smallbox)
+
+    });
+
+    document.body.appendChild(container3)
+
 
 
 }
-getdata();
+
+
+
+search.addEventListener('click', getProducts)
